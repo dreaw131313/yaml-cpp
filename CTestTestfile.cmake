bdd4de6065a4f8a407f29f82338d31d67a543f14
Dreaw <jezierski.michal13@gmail.com>
2023-09-06 23:11:06 +0200

    Created VS 2022 solution for project

---
+++ CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/TRY_Engine/dev/Try/Libraries/yaml_cpp/yaml_cpp
+# Build directory: D:/TRY_Engine/dev/Try/Libraries/yaml_cpp/yaml_cpp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("util")
